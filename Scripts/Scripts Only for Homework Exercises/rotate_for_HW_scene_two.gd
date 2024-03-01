0eb50d3ee8c947efccf0d5c9be1b4ce83c07559b
owenbadgley <143543330+owenbadgley@users.noreply.github.com>
2024-03-01 16:13:32 -0500

    problem 2

--- Scripts/Scripts Only for Homework Exercises/rotate_for_HW_scene_two.gd
+++ Scripts/Scripts Only for Homework Exercises/rotate_for_HW_scene_two.gd
@@ -18,5 +18,9 @@ func _process(delta):
 	pass
 
 
+func _on_area_2d_body_entered(body):
+	is_rotating = true
+	pass # Replace with function body.
 
-
+func _on_area_2d_body_exited(body):
+	is_rotating = false
